5a4b6f4e4a757963ef23da8f0cf9a773177f559d
Ross Singer <rossfsinger@gmail.com>
2007-06-28 17:32:16 +0000

    Updated to deal with the new Service refactoring

--- app/controllers/link_router_controller.rb
+++ app/controllers/link_router_controller.rb
@@ -1,8 +1,8 @@
+require 'cgi'
 class LinkRouterController < ApplicationController
-  require 'cgi'
   def index
     svc_type = ServiceType.find(params[:id])    
-    redirect_to svc_type.service_response.service.response_url(svc_type.service_response)
+    redirect_to ServiceList.get(svc_type.service_response.service).response_url(svc_type.service_response)
     clickthrough = Clickthrough.new
     clickthrough.request_id = svc_type.request_id
     clickthrough.service_response_id = svc_type.service_response_id
