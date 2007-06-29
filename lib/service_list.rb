b90ea9d6906bcc55173cc3d3f6240f1f2ee9cd9d
Jonathan Rochkind <rochkind@jhu.edu>
2007-06-29 13:13:43 +0000

    Check for a service name referenced but not loaded from services.yml, and raise a meaningful exception.

--- lib/service_list.rb
+++ lib/service_list.rb
@@ -3,8 +3,17 @@ class ServiceList
   @@services = nil
   
   def self.get(name)
-    @@services = YAML.load_file(RAILS_ROOT+"/config/services.yml") unless @@services   
-    require 'service_adaptors/'+@@services[name]["type"].underscore   
-    return Kernel.const_get(@@services[name]["type"]).new(@@services[name].merge({"id"=>name}))
-  end  
+    @@services = YAML.load_file(RAILS_ROOT+"/config/services.yml") unless @@services
+
+    if (@@services[name].nil?)
+      raise NameError.new("No such service named #{name} has been loaded. Check config/services.yml", name)
+    end
+    
+    require 'service_adaptors/'+@@services[name]["type"].underscore
+    
+    className = @@services[name]["type"]
+    classConst = Kernel.const_get(className)\
+    
+    return classConst.new(@@services[name].merge({"id"=>name}))
+  end
 end
