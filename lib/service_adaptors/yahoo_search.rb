2c042ac5471c1b0c9d1b13390b8d034c73aad47a
Ross Singer <rossfsinger@gmail.com>
2007-06-29 16:19:44 +0000

    Updated to use add_service_response and removed all "Relevant/Irrelevant" site logic to be handled further downstream

--- lib/service_adaptors/yahoo_search.rb
+++ lib/service_adaptors/yahoo_search.rb
@@ -8,34 +8,10 @@ class YahooSearch < Service
     links = self.do_query(query)
     return request.dispatched(self, false) unless links.is_a?(Array)
     links.each do | link |
-      link[:relevant] = false
-      unless IrrelevantSite.is_irrelevant?(link[:url])
-        if relevant = RelevantSite.is_relevant?(link[:url])
-          unless relevant.is_a?(RelevantSite)
-            link.merge!(relevant.get_services(link, request))          
-          end
-          link[:relevant] = true
-        end
-      end
       value_text = {:description => link[:description],
         :url => link[:url]
       }
-      value_text.merge!(link[:fulltext]) if link[:fulltext]
-        
-      svc_resp = nil
-      unless svc_resp = ServiceResponse.find_by_service_and_key_and_value_string(self.id, link[:title], link[:hash])
-        svc_resp = ServiceResponse.create(:service=>self.id,:key=>link[:title],:value_string=>MD5.new(link[:url]),:value_text=>value_text.to_yaml)
-      end	
-      svc_type = request.service_types.create(:service_response_id=>svc_resp.id, :service_type=>'web_link') unless request.service_types.find(:first, :conditions=>["service_response_id = ? AND service_type = ?", svc_resp.id, 'web_link'])                  
-      if link[:relevant]
-        svc_type = request.service_types.create(:service_response_id=>svc_resp.id, :service_type=>'relevant_link') unless request.service_types.find(:first, :conditions=>["service_response_id = ? AND service_type = ?", svc_resp.id, 'relevant_link'])      
-      end
-      if link[:fulltext]
-        svc_type = request.service_types.create(:service_response_id=>svc_resp.id, :service_type=>'fulltext') unless request.service_types.find(:first, :conditions=>["service_response_id = ? AND service_type = ?", svc_resp.id, 'fulltext'])      
-        if link[:related_links] == 'true'
-          svc_type = request.service_types.create(:service_response_id=>svc_resp.id, :service_type=>'related') unless request.service_types.find(:first, :conditions=>["service_response_id = ? AND service_type = ?", svc_resp.id, 'related'])        
-        end        
-      end
+      request.add_service_response({:service=>self,:key=>link[:title],:value_string=>link[:hash],:value_text=>value_text.to_yaml},['web_link'])
     end
     return request.dispatched(self, true)    
   end
