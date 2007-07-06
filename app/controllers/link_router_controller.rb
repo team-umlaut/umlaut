4e273aee69d5bbe5f7c86b0f74dff765df853fbb
Jonathan Rochkind <rochkind@jhu.edu>
2007-07-06 15:24:06 +0000

    fixed bug, service_response.service should have been service_response.service_id

--- app/controllers/link_router_controller.rb
+++ app/controllers/link_router_controller.rb
@@ -1,8 +1,10 @@
 require 'cgi'
 class LinkRouterController < ApplicationController
   def index
+    #require 'ruby-debug'
+    #debugger
     svc_type = ServiceType.find(params[:id])    
-    redirect_to ServiceList.get(svc_type.service_response.service).response_url(svc_type.service_response)
+    redirect_to ServiceList.get(svc_type.service_response.service_id).response_url(svc_type.service_response)
     clickthrough = Clickthrough.new
     clickthrough.request_id = svc_type.request_id
     clickthrough.service_response_id = svc_type.service_response_id
