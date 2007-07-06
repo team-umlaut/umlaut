b57f5c486fd1626d1d50ecff4e606c7c84ad1b3b
Jonathan Rochkind <rochkind@jhu.edu>
2007-07-06 21:56:43 +0000

    fixed bug when not enough metadata for service to operate, no_op gracefully

--- lib/service_adaptors/yahoo_search.rb
+++ lib/service_adaptors/yahoo_search.rb
@@ -1,30 +1,42 @@
 class YahooSearch < Service
   require 'md5'
   require 'json/lexer'
+  
+  required_config_params :url, :api_key
   attr_reader :url, :api_key
+  
   def handle(request)
     raise "YahooSearch: Url or API key are nil. They must be filled out in config for YahooSearch service. The password should be an API key from Yahoo: https://developer.yahoo.com/wsregapp/index.php" if self.url.nil? or self.api_key.nil?
-    query = self.build_query(request.referent)
-    links = self.do_query(query)
-    return request.dispatched(self, false) unless links.is_a?(Array)
-    links.each do | link |
-      value_text = {:description => link[:description],
-        :url => link[:url]
-      }
-      request.add_service_response({:service=>self,:key=>link[:title],:value_string=>link[:hash],:value_text=>value_text.to_yaml},['web_link'])
+
+    debugger
+    catch (:no_op) do
+      query = self.build_query(request.referent)
+      links = self.do_query(query)
+      return request.dispatched(self, false) unless links.is_a?(Array)
+      links.each do | link |
+        value_text = {:description => link[:description],
+          :url => link[:url]
+         }
+        request.add_service_response({:service=>self,:key=>link[:title],:value_string=>link[:hash],:value_text=>value_text.to_yaml},['web_link'])
+      end
+      return request.dispatched(self, true)
     end
-    return request.dispatched(self, true)    
+    # If we made it here, we have a no-op. Okay, we didn't do anything,
+    # but we chose not to, that's success. 
+    return request.dispatched(self, true)
   end
   
   def build_query(rft)
+    debugger
     query = ""
     metadata = rft.metadata
     ['atitle','title','jtitle','btitle','au','aulast', 'date'].each do | field |
       query << ' "'+metadata[field]+'"' if metadata[field]
     end
 	if query == ""
-		return false
-	end   
+    # We can't and shouldn't perform the service, just give up.  
+    throw :no_op
+  end
     return "appid=#{@api_key}&query="+CGI::escape(query)+"&results=50&start=1&output=json"    
   end
   
@@ -34,7 +46,7 @@ class YahooSearch < Service
     yahoo_uri = URI.parse(self.url+yws)
 
     # send the request
-    http = Net::HTTP.new(yahoo_uri.host, 80)  
+    http = Net::HTTP.new(yahoo_uri.host, 80)
     http_response = http.send_request('POST', yahoo_uri.path + '?' + query)
     begin
       json = JSON::Lexer.new(http_response.body).nextvalue
