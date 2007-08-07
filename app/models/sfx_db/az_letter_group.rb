933a89593f9e46d303d01d8800690f0f0f581a3c
Jonathan Rochkind <rochkind@jhu.edu>
2007-08-07 18:15:16 +0000

    sorry, forgot to add before

---
+++ app/models/sfx_db/az_letter_group.rb
@@ -0,0 +1,9 @@
+module SfxDb
+  class AzLetterGroup < SfxDbBase
+    self.table_name = 'AZ_LETTER_GROUP_VER3'
+    self.primary_key = 'AZ_LETTER_GROUP_VER3_ID'
+
+    belongs_to :az_title,
+               :foreign_key => 'AZ_TITLE_VER3_ID'
+  end
+end
