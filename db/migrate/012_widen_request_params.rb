f1cd3840f98b05c68c6ffcef94ba726c64e82cd2
Jonathan Rochkind <rochkind@jhu.edu>
2007-08-30 19:21:15 +0000

    column was too narrow, cutting content off

---
+++ db/migrate/012_widen_request_params.rb
@@ -0,0 +1,9 @@
+class WidenRequestParams < ActiveRecord::Migration
+  def self.up
+    change_column :requests, :params, :string, :limit => 2048
+  end
+
+  def self.down
+    change_column :requests, :params, :limit => 1024
+  end
+end
