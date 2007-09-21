1f4895faef95a93461c52808e08c45edab343ed1
Jonathan Rochkind <rochkind@jhu.edu>
2007-09-21 20:35:42 +0000

    model for tracking sfx urls

---
+++ app/models/sfx_url.rb
@@ -0,0 +1,22 @@
+# Just an indexed list of URLs extracted from SFX, urls we believe are
+# sfx-controlled. Kind of an ugly hack, kind of duplicates the local journal
+# index if in use, but we need it to suppress catalog URLs if they duplicate
+# what SFX ought to control. 
+class SfxUrl < ActiveRecord::Base
+
+  # Pass in a string, we tell you if we think SFX controls this URL--
+  # that is, if the SFX KB handles resources at this URL, or not. 
+  # It's really just a guess for a bunch of reasons, but best we can
+  # do. We just check hostname, which could create false positives.
+  # Checking entire URL won't work. 
+  # Lots of things in SFX could create false negatives. 
+  def self.sfx_controls_url?(url)
+    uri = URI.parse(url)
+    host = uri.host
+
+    # If URI was malformed, just punt and say no.
+    return false unless host
+    
+    SfxUrl.find(:all, :conditions => ["url = ?", host]).length > 0
+  end
+end
