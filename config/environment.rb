36c7f29e33b7c0c015c5b3f44684a77bca3095dc
Jonathan Rochkind <rochkind@jhu.edu>
2008-01-10 19:35:58 +0000

    more clear comments

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,13 @@
-# Be sure to restart your web server when you modify this file.
+# Umlaut implementors: You should not need to modify this file for local
+# implementation. Local configuration goes in 
+# config/umlaut_config/environment.rb instead.
 
-# Uncomment below to force Rails into production mode when 
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
+# This file includes Umlaut defaults that are cross-installation, that
+# are part of the app itself. 
 
 # Specifies gem version of Rails to use when vendor/rails is not present
+# Do not freeze to a particular Gem, but umlaut is tested/developed with
+# 1.2.1 . I think. 
 RAILS_GEM_VERSION = '1.2.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
