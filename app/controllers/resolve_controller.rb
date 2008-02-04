a7db47b56382b881a3e09dcaf8c618829d1b76ec
Jonathan Rochkind <rochkind@jhu.edu>
2008-02-04 15:49:05 +0000

    make sure we have layout for any error page

--- app/controllers/resolve_controller.rb
+++ app/controllers/resolve_controller.rb
@@ -406,7 +406,7 @@ class ResolveController < ApplicationController
 
   
   def rescue_action_in_public(exception)
-    render :template => "error/resolve_error"    
+    render(:template => "error/resolve_error", :layout => AppConfig.param("resolve_layout", "resolve_basic")) 
   end  
 
   # Obsolete, I think. 
