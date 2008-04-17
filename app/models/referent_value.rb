52f0cbfb9cc0ce9ae82483d61825244b84f38b1a
Jonathan Rochkind <rochkind@jhu.edu>
2008-04-17 15:04:29 +0000

    slight improvement to normalization

--- app/models/referent_value.rb
+++ app/models/referent_value.rb
@@ -1,8 +1,14 @@
 class ReferentValue < ActiveRecord::Base
   belongs_to :referent
 
-
+  # Class method to normalize a string for normalized_value attribute. 
+  # Right now normalization is just downcasing. Only
+  # metadata values should be normalized (ie, not 'identifier' or 'format').
+  # identifier and format shoudl be stored in normalized_value unchanged.
   def self.normalize(input)
-      return input.chars.downcase.to_s
+      # 'chars' is neccesary for unicode.
+      # normalized_value column only holds 254 bytes.. 
+      return input.chars.downcase.to_s[0..254]
   end
+  
 end
