95ac49b17b3febfb22e2885e5356dd41b4d45766
Jonathan Rochkind <rochkind@jhu.edu>
2008-06-19 15:57:08 +0000

    javascripts to support embedding of umlaut in other tools using partial html action

---
+++ public/javascripts/embed/umlaut-embed.js
@@ -0,0 +1,41 @@
+  //This script depends on jsr_class.js being imported by client too. 
+  
+  // Loads a URL using the JSONscriptRequest to do a cross-domain AJAX. 
+  function load_jsonp_url(url) {
+      bObj = new JSONscriptRequest( url );
+      bObj.buildScriptTag();
+      bObj.addScriptTag();
+  }
+
+  
+  // Callback called by jsonp technique. 
+  function umlaut_partial_load_callback(jsonData) {
+    var sections = jsonData.partial_html_sections.html_section;
+    for ( var i = sections.length-1 ; i>=0 ; --i ) {
+      var section = sections[i];
+      
+      var host_div_id = umlaut_section_map[section.id];
+      if ( host_div_id ) {
+        var host_div = document.getElementById( host_div_id );
+        if ( host_div ) {
+          host_div.innerHTML = section.html_content; 
+        }        
+      }      
+    }
+    //Now do we need a reload?
+    if ( jsonData.partial_html_sections.in_progress ) {
+      refresh_path = jsonData.partial_html_sections.in_progress.refresh_url_path;
+    
+      refresh_seconds = jsonData.partial_html_sections.in_progress.requested_wait_seconds;
+    
+      refresh_url = umlaut_base + refresh_path + "&amp;umlaut.response_format=jsonp&amp;umlaut.jsonp_callback=umlaut_partial_load_callback";
+      
+      window.setTimeout( "load_jsonp_url('" + refresh_url + "')", 4000 );
+    }
+  }
+
+  
+  request = umlaut_base + '/resolve/partial_html_sections?umlaut.response_format=jsonp&amp;umlaut.jsonp=umlaut_partial_load_callback&amp;' + umlaut_openurl_kev_co;
+  
+  load_jsonp_url( request );
+  
