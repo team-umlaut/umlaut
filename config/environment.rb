d25803ddc0e5276d246e45803b0fd9b49aeeb8dc
Jonathan Rochkind <rochkind@jhu.edu>
2008-10-15 14:10:51 +0000

    add service_adaptors directory to rails auto load paths

--- config/environment.rb
+++ config/environment.rb
@@ -142,7 +142,7 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # Umlaut does. 
   config.load_paths += %W( #{RAILS_ROOT}/lib/referent_filters )
-
+  config.load_paths += %W( #{RAILS_ROOT}/lib/service_adaptors )
   
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
@@ -216,8 +216,6 @@ Rails::Initializer.run do |config|
   config.app_config.rfr_ids[:opensearch] = "info:sid/umlaut.code4lib.org:opensearch"
   config.app_config.rfr_ids[:citation] = "info:sid/umlaut.code4lib.org:citation"
   config.app_config.rfr_ids[:azlist] = 'info:sid/umlaut.code4lib.org:azlist'
-
-  
 
   
   # SFX Targets and other urls that we know have a problem with
