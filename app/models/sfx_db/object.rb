958b9873656c0dd8c6b44493e62a3eb741254c0e
Jonathan Rochkind <rochkind@jhu.edu>
2008-12-02 16:43:12 +0000

    explicitly specify class name with module in all associations, to try and get rid of mysterious error in logs

--- app/models/sfx_db/object.rb
+++ app/models/sfx_db/object.rb
@@ -8,24 +8,27 @@ module SfxDb
     has_many  :titles,
               :foreign_key => 'OBJECT_ID',
               :class_name => 'SfxDb::Title'
+              
     has_many   :publishers,
               :foreign_key => 'OBJECT_ID',
               :class_name => 'SfxDb::Publisher'
+              
     has_many  :az_titles,
-              :foreign_key=>'OBJECT_ID'
+              :foreign_key=>'OBJECT_ID',
+              :class_name => 'SfxDb::AzTitle'
 
     has_many :primary_isbns,
-             :class_name => 'Isbn',
+             :class_name => 'SfxDb::Isbn',
              :foreign_key => 'OBJECT_ID',
              :conditions => "ISBN_HIERARCHY = 'PRIMARY'"
 
     has_many :primary_issns,
-             :class_name => 'Issn',
+             :class_name => 'SfxDb::Issn',
              :foreign_key => 'OBJECT_ID',
              :conditions => "ISSN_HIERARCHY = 'PRIMARY'"
 
     has_many :main_titles,
-             :class_name => 'Title',
+             :class_name => 'SfxDb::Title',
              :foreign_key => 'OBJECT_ID',
              :conditions => "TITLE_TYPE = 'main'"
   end
