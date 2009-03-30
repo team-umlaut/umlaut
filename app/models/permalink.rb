5c8d40000cdfbf5c72d515140507120c861763ff
Jonathan Rochkind <rochkind@jhu.edu>
2009-03-30 15:01:31 +0000

    No longer add a tag-uri for each referent. Since we aren't caching referents on rft_id anyway, this doesn't accomplish anything except confusion.

--- app/models/permalink.rb
+++ app/models/permalink.rb
@@ -6,8 +6,7 @@ class Permalink < ActiveRecord::Base
   belongs_to :referent
 
   # You should create Permalinks with this. Pass in a referent and referrer
-  #. Will save  permalink to db, and create a ReferentValue for an identifier 
-  # with tag-uri based on the permalink. 
+  #. Will save  permalink to db
   def self.new_with_values!(rft, rfr)
     permalink = Permalink.new
     
@@ -18,27 +17,10 @@ class Permalink < ActiveRecord::Base
     permalink.context_obj_serialized = permalink.referent.to_context_object.xml
 
     permalink.save!
-
-    permalink.add_tag_uri!(rft)
     
     return permalink
   end
 
-  # Adds a ReferentValue to argument representing identifier with tag-uri for
-  # self. ReferentValue does end up saved. 
-  def add_tag_uri!(referent)
-    val = ReferentValue.new
-    val.key_name = 'identifier'
-    val.value = self.tag_uri
-    val.normalized_value = val.value
-    referent.referent_values << val
-  end
-  
-  def tag_uri
-    require "socket"
-    host = Socket.gethostname  
-    return 'tag:'+host+','+self.created_on.year.to_s+'-'+self.created_on.month.to_s+'-'+self.created_on.day.to_s+':'+self.id.to_s
-  end
 
   # Takes the XML stored in self.context_obj_serialized, and turns it back
   # into an OpenURL ContextObject
