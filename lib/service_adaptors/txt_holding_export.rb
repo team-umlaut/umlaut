12970f91173e04fd620b524f683a0ef2bfe51959
Jonathan Rochkind <rochkind@jhu.edu>
2009-05-13 22:10:18 +0000

    A lot more concise way to write that.

--- lib/service_adaptors/txt_holding_export.rb
+++ lib/service_adaptors/txt_holding_export.rb
@@ -1,8 +1,8 @@
 class TxtHoldingExport < EmailExport
 
   def initialize(config)
-    self.instance_variable_set("@controller", "export_email") if self.instance_variable_get("@controller").nil?
-    self.instance_variable_set("@ajax_id", "txt") if self.instance_variable_get("@ajax_id").nil?
+    @controller ||= "export_email")
+    @ajax_id ||= "txt"
     super(config)
   end
 
