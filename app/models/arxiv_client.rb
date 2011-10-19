b4e98cacd31c2063eac44d78c3e6b6aa4feb15ef
Jonathan Rochkind <jonathan@dnil.net>
2011-10-19 15:09:48 -0400

    remove unfinished stub for ArxivClient, never used.

--- app/models/arxiv_client.rb
+++
@@ -1,6 +0,0 @@
-class ArxivClient
-
-  def make_interesting(link, context_object)
-    return {:fulltext_link=>{:type => "arXiv", :source=>'ArXiv.org', :source_id=>nil, :display_text =>"arXiv.org",:url => link[:url]}}
-  end
-end
