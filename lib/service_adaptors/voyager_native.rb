3a7e53ba834cd204b7fbc890cedb315475b72574
Jonathan Rochkind <jonathan@dnil.net>
2011-10-24 17:28:38 -0400

    remove old voyager clients, not supported, haven't been used in a while, use old code

--- lib/service_adaptors/voyager_native.rb
+++
@@ -1,8 +0,0 @@
-# FIXME This service may not be working
-require 'service_adaptors/voyager'
-class VoyagerNative < Voyager
-  attr_reader :username, :password, :dsn
-  def init_holdings_client
-    return VoyagerClient.new(self)
-  end
-end
