d1f04202037c3f4b1d7fcd4cee7f4718def8b71f
Jonathan Rochkind <jonathan@dnil.net>
2011-10-24 17:30:24 -0400

    remove frozen SRU gem; can be added as ordinary gem dependency if needed, but we've removed everything that used it.

--- vendor/plugins/sru-0.0.3/lib/sru.rb
+++
@@ -1,8 +0,0 @@
-# allow people to require sru and get all the goodies
-
-require 'sru/client'
-require 'sru/explain'
-require 'sru/search_retrieve'
-require 'sru/scan'
-require 'sru/term'
-require 'sru/exception'
--- vendor/plugins/sru-0.0.3/lib/sru/exception.rb
+++
@@ -1,5 +0,0 @@
-# wrapper for exceptions that happen during an sru operation
-module SRU
-  class Exception < RuntimeError 
-  end
-end
