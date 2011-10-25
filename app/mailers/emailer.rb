b0310a516d76d01265af609134d7f7901dc7fd05
Jonathan Rochkind <jonathan@dnil.net>
2011-10-25 13:46:48 -0400

    more Rails3 ActionMailer

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -11,22 +11,22 @@ class Emailer < ActionMailer::Base
     
     mail(:to => recipient, 
          :from => AppConfig.param("from_email_addr"), 
+         :'Reply-to' => AppConfig.param("from_email_addr"),
          :subject => "#{AppConfig.param("app_name")} result: #{find_good_title(user_request.referent)}")
   end
   
   def short_citation(recipient, user_request, location, call_number)
-    email = AppConfig.param("from_email_addr")
-    @recipients   = recipient
-    @from         = email
-    headers         "Reply-to" => email
-    @subject      = "#{AppConfig.param("app_name")} result"
-    @sent_on      = Time.now
-    @content_type = "text/plain"
+    
  
-    @body["title"]  = find_good_title(user_request.referent)
-    @body["location"]  = location
-    @body["call_number"]  = call_number
-    @body["user_request"] = user_request
+    @title  = find_good_title(user_request.referent)
+    @location  = location
+    @call_number  = call_number
+    @user_request = user_request
+    
+    mail(:to => recipient, 
+         :from => AppConfig.param("from_email_addr"),
+         :'Reply-to' => AppConfig.param("from_email_addr"), 
+         :subject => "#{AppConfig.param("app_name")} result")
 
   end
 
