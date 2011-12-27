eecb64ec1e3197e8f69e0663039ee94a25914c1a
Jonathan Rochkind <jonathan@dnil.net>
2011-12-27 16:36:30 -0500

    add umlaut's paths to engine startup config

--- lib/umlaut.rb
+++ lib/umlaut.rb
@@ -1,6 +1,23 @@
+require 'umlaut/routes'
+
 module Umlaut
   class Engine < Rails::Engine
     engine_name "umlaut"
+    
+    # we store some things in non-standard subdirs, add em to path.
+    #
+    # We EAGER load em all to try and handle threading issues. 
+    
+    config.autoload_paths << File.join(self.root, "lib", "referent_filters")
+    config.eager_load_paths << File.join(self.root, "lib", "referent_filters")
+    
+    config.autoload_paths << File.join(self.root, "lib", "service_adaptors")
+    config.eager_load_paths << File.join(self.root, "lib", "service_adaptors")
+    
+    # Ane make 'lib' dir auto-loaded, because we have legacy Rails2 code
+    # that assumes it.
+    config.autoload_paths << File.join(self.root, "lib")
+    config.eager_load_paths << File.join(self.root, "lib")
     
     # This makes our rake tasks visible.
     rake_tasks do
