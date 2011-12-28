e0283923e20126f8f9128e696484b6f2ec4dc019
Jonathan Rochkind <jonathan@dnil.net>
2011-12-28 12:22:05 -0500

    nice ending message

--- lib/generators/umlaut/install_generator.rb
+++ lib/generators/umlaut/install_generator.rb
@@ -2,6 +2,8 @@ require 'rails/generators'
 
 module Umlaut
   class Install < Rails::Generators::Base
+    include ActionView::Helpers::TextHelper
+    
     def install
       generate("umlaut:class_cache_conf")
       
@@ -9,7 +11,16 @@ module Umlaut
       
       generate("umlaut:service_conf")
       
-      rake("umlaut:install:migrations")      
+      rake("umlaut:install:migrations")
+      
+      say("              Umlaut installed, now:", :yellow)
+      $stdout.puts(
+        "              " +
+        word_wrap("After setting up your 'development' database in config/databases.yml, run `rake db:migrate`", :line_width => 60).
+          split("\n").
+          join("\n              ")
+        )
+      
     end
   end  
 end
