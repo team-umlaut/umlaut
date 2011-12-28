5da3bb1762bcddc41071868e41498b649fa6de01
Jonathan Rochkind <jonathan@dnil.net>
2011-12-28 15:28:59 -0500

    only/except options for install generator

--- lib/generators/umlaut/install_generator.rb
+++ lib/generators/umlaut/install_generator.rb
@@ -3,43 +3,59 @@ require 'rails/generators'
 module Umlaut
   class Install < Rails::Generators::Base
     source_root File.join(Umlaut::Engine.root)
+    
+    class_option :only, :type=>:array
+    class_option :except, :type=>:array
             
     include ActionView::Helpers::TextHelper
     
-    def no_class_reloading
-      /^((\s*)config\.cache_classes +\= +false\s*)$/
-      gsub_file("config/environments/development.rb", /^(\s*)config\.cache_classes +\= +false\s*$/) do |match|
-        # for some reason we can't make access to $1 in here from the above
-        # regexp work, so we need to match again
-        match =~ /^(\s*)/        
-        <<-EOS
+    def config_cache_classes
+      guarded(:config_cache_classes) do
+        /^((\s*)config\.cache_classes +\= +false\s*)$/
+        gsub_file("config/environments/development.rb", /^(\s*)config\.cache_classes +\= +false\s*$/) do |match|
+          # for some reason we can't make access to $1 in here from the above
+          # regexp work, so we need to match again
+          match =~ /^(\s*)/        
+          <<-EOS
 #{$1}#
 #{$1}# UMLAUT: Umlaut's use of threading is not compatible with class
 #{$1}# reloading, even in development. Umlaut requires true here.
 #{$1}# Rails 3.2 _might_ let you get away with false when it comes out.
 #{$1}config.cache_classes = true
-          EOS
+            EOS
+        end
       end
-    end        
-  
-    def inject_blacklight_routes
-      route("Umlaut::Routes.new(self).draw")
     end
     
-    def generate_service_conf_skeleton
-      copy_file("lib/generators/templates/umlaut_services.yml", "config/umlaut_services.yml")
+  
+    def routes
+      guarded(:routes) do
+        route("Umlaut::Routes.new(self).draw")
+      end
+    end
+    
+    def umlaut_services_skeleton
+      guarded(:umlaut_services_skeleton) do
+        copy_file("lib/generators/templates/umlaut_services.yml", "config/umlaut_services.yml")
+      end
     end
     
     def migrations
-      rake("umlaut:install:migrations")
+      guarded(:migrations) do
+        rake("umlaut:install:migrations")
+      end
     end
     
     def asset_hooks
-      generate("umlaut:asset_hooks")
+      guarded(:asset_hooks) do
+        generate("umlaut:asset_hooks")
+      end
     end
     
     def local_umlaut_controller
-      copy_file("app/controllers/umlaut_controller.rb")
+      guarded(:local_umlaut_controller) do
+        copy_file("app/controllers/umlaut_controller.rb")
+      end
     end
       
     def post_install_message            
@@ -52,5 +68,18 @@ module Umlaut
           )
         
     end
+    
+    no_tasks do
+      def guarded(section, &block)
+        if (options[:only].nil? || options[:only].include?(section.to_s)) &&
+           (options[:except].nil? || ! options[:except].include?(section.to_s))
+          yield
+        else
+          say_status("skipped", section.to_s, :blue)
+        end
+        
+      end
+    end
+    
   end  
 end
