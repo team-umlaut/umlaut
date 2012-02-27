5f08a4c2288457cdf7c6569bf674336d12c4e8ee
Jonathan Rochkind <jonathan@dnil.net>
2012-02-27 11:25:36 -0500

    make engine tests find fixtures when run in dummy app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
+ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__) 
+
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
