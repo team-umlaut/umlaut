847b22e3a2609fb790621e2c758ab3da1acca3d8
Jonathan Rochkind <jonathan@dnil.net>
2012-03-01 16:41:45 -0500

    some cheesy auth and protection of admin reports

--- app/controllers/admin/service_errors_controller.rb
+++ app/controllers/admin/service_errors_controller.rb
@@ -1,5 +1,19 @@
 module Admin
   class ServiceErrorsController < UmlautController
+    # by default, force ssl connection in production env, but can be
+    # config'd. 
+    force_ssl if umlaut_config.lookup!("admin.force_ssl", Rails.env.production? )
+    # Cheesy way to do auth from config single or multiple. Alternately,
+    # you might want to protect at apache level, for instance for shib
+    # or SSO integration
+    if (admin_auth = umlaut_config.lookup!("admin.auth"))
+      before_filter lambda {
+        authenticate_or_request_with_http_basic do |login, password|                    
+          login == admin_auth[:username] && password == admin_auth[:password]
+        end
+      }
+    end
+    
     
     def index
       # grab the earliest dispatch to see how far back our db goes
@@ -31,7 +45,7 @@ module Admin
       # kaminari page/per
       @offset = params[:offset].to_i 
       @limit = params[:per_page].to_i 
-      @limit = 10 if @limit = 0
+      @limit = 10 if @limit == 0
       
       @dispatched_services = errors_base.order("updated_at DESC").
         limit(@limit).offset(@offset)             
