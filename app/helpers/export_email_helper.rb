da72efad9ac2e1b1ecaf3441831983ab246240dd
Scot Dalton <scotdalton@gmail.com>
2012-09-26 15:57:13 -0400

    Name change for location method which appears to conflict with and internal rails method name.  Refactor email JS into CoffeeScript and moved the error display into a helper.

--- app/helpers/export_email_helper.rb
+++ app/helpers/export_email_helper.rb
@@ -3,18 +3,14 @@ module ExportEmailHelper
   
   def formatted_txt_holding_status(view_data)
     output = ""
-
     output << view_data[:collection_str] if view_data[:collection_str]
     output << " " + view_data[:call_number] if view_data[:call_number]
     output << " " + view_data[:status] if view_data[:status]
-    
     return output
   end
 
   def formatted_html_holding_status(view_data)
     output = "".html_safe
-
-    
     if view_data[:collection_str]
       output << content_tag("span", view_data[:collection_str], :class => "collection") 
     end
@@ -24,11 +20,19 @@ module ExportEmailHelper
     if view_data[:status]
       output << " ".html_safe << content_tag("span", view_data[:status], :class => "status")
     end
-      
     return output
   end
 
-
-
-  
+  def validation_error(errors)
+    return content_tag(:div, :class => "validation_errors") do
+      return content_tag(:span, "Please provide the following:") + 
+        content_tag(:ul) do 
+          e = "".html_safe
+          errors.each do |error|
+            e << content_tag(:li, error)
+          end
+          e
+        end
+    end
+  end
 end
