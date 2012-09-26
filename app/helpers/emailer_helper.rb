542a35b70d44f72ed9c94c866e1fcd1be4951f0a
Scot Dalton <scotdalton@gmail.com>
2012-09-26 16:37:23 -0400

    Refactor for Twitter Bootstrap and clean up code a bit.

--- app/helpers/emailer_helper.rb
+++ app/helpers/emailer_helper.rb
@@ -3,20 +3,13 @@ module EmailerHelper
   
   # returns a plain text short citation
   def brief_citation(request, options = {})
-    
     options[:include_labels] ||= false
-
     rv =""
-    
-    
     cite = request.referent.to_citation 
-
     title = truncate(cite[:title].strip, :length => 70,  :seperator => '...')
-    
     rv << (cite[:title_label].strip + ": ")if options[:include_labels]
     rv << title
     rv << "\n"
-    
     if cite[:author]  
       rv << "Author: " if options[:include_labels]
       rv << cite[:author].strip
@@ -27,7 +20,6 @@ module EmailerHelper
       rv << cite[:subtitle].strip
       rv << "\n"
     end
-
     pub = []
     pub << date_format(cite[:date]) unless cite[:date].blank?	
     pub << 'Vol: '+cite[:volume].strip unless cite[:volume].blank?
@@ -37,7 +29,6 @@ module EmailerHelper
       rv << "Published: " if options[:include_labels]
       rv << pub.join('  ')
     end
-    
     return rv  
   end
 end
