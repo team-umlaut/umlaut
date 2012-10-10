e0e57a1dd6e98e8fde20b90fda133b539bf51e14
Scot Dalton <scotdalton@gmail.com>
2012-10-10 14:11:01 -0400

    Update test/dummy db configs and migration to raise an error if it's not explicitly a test_instance.

--- test/dummy/db/migrate/20120927163304_sfx4_global.rb
+++ test/dummy/db/migrate/20120927163304_sfx4_global.rb
@@ -1,4 +1,5 @@
-# Create a test SFX Global DB.
+# This is used for SFX search testing.
+# DO NOT USE THIS FOR ANYTHING LIKE A REAL SFX DATABASE.
 class Sfx4Global < ActiveRecord::Migration
   def connection
     ActiveRecord::Base.establish_connection(:sfx4_global)
@@ -6,10 +7,17 @@ class Sfx4Global < ActiveRecord::Migration
     ActiveRecord::Base.connection
   end
 
-  def change    
+  def change
+    unless_testing_raise_error
     create_table "KB_OBJECTS", {:id => false} do |t|
       t.integer "OBJECT_ID", :default => 0, :null => false, :limit => 8
     end
     execute "ALTER TABLE KB_OBJECTS ADD PRIMARY KEY (OBJECT_ID);"
+  end
+
+  def unless_testing_raise_error
+    unless ActiveRecord::Base.configurations["sfx4_global"]["test_instance"]
+      raise SecurityError.new("Danger! This is for mock SFX testing only! Do not run this migration against any sort of real SFX database.")
+    end
   end
 end
