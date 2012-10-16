715f3919c53a2d705dde6b8e452bad2af60b9b86
Scot Dalton <scotdalton@gmail.com>
2012-10-15 21:47:16 -0400

    Update to get test to run for real. Not sure what I was thinking before.

--- app/models/sfx4/local/base.rb
+++ app/models/sfx4/local/base.rb
@@ -4,10 +4,11 @@ module Sfx4
       # Was a SFX DB connection set in database.yml to connect directly to sfx?
       def self.connection_configured?
         config = ActiveRecord::Base.configurations["sfx_db"]
-        (not (config.nil? or config.blank? or config[:adapter].blank?))
+        (not (config.nil? or config.blank? or config["adapter"].blank?))
       end
 
       self.establish_connection :sfx_db if self.connection_configured?
+
       # ActiveRecord likes it when we tell it this is an abstract
       # class only. 
       self.abstract_class = true 
