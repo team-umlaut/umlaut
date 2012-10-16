d0080214729802ccc64f7fed5c7e48278de086b4
Scot Dalton <scotdalton@gmail.com>
2012-10-15 22:22:19 -0400

    Update schema version.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120927164040) do
+ActiveRecord::Schema.define(:version => 20120402191628) do
 
   create_table "clickthroughs", :force => true do |t|
     t.integer  "request_id",          :default => 0, :null => false
