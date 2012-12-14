e25880c8f8032b710767d0c3585543788f82b753
Scot Dalton <scotdalton@gmail.com>
2012-12-14 11:08:28 -0500

    Prevent default action on toggles.

--- app/assets/javascripts/umlaut/expand_contract_toggle.js
+++ app/assets/javascripts/umlaut/expand_contract_toggle.js
@@ -8,7 +8,8 @@
    pipline fingerprinted. sorry, best way to make it work!
 */
 jQuery(document).ready(function($) {
-  $(".expand_contract_toggle").live("click", function() {
+  $(".expand_contract_toggle").live("click", function(event) {
+    event.preventDefault();
     var content = $(this).next(".expand_contract_content");
     var icon = $(this).parent().find('img.toggle_icon');
     if (content.is(":visible")) {
--- app/assets/javascripts/umlaut/simple_visible_toggle.js
+++ app/assets/javascripts/umlaut/simple_visible_toggle.js
@@ -1,6 +1,7 @@
 /* simple_visible_toggle.js.  Used for toggling visibility of error information. Can possibly be combined with more powerful expand_contract_toggle.js */
 jQuery(document).ready(function($) {
-    $("a.simple_visible_toggle").live("click", function() {
+    $("a.simple_visible_toggle").live("click", function(event) {
+      event.preventDefault();
        $(this).next().toggle(); 
     });
 });
