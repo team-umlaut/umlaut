9d6ed3c90245f2e270a18efcdb533eb011582589
Scot Dalton <scotdalton@gmail.com>
2012-12-19 15:48:04 -0500

    Fix expand contract bug.

--- app/assets/javascripts/umlaut/expand_contract_toggle.js
+++ app/assets/javascripts/umlaut/expand_contract_toggle.js
@@ -11,13 +11,13 @@ jQuery(document).ready(function($) {
   $(".expand_contract_toggle").live("click", function(event) {
     event.preventDefault();
     var content = $(this).next(".expand_contract_content");
-    var icon = $(this).parent().find('img.toggle_icon');
+    var icon = $(this).parent().find('i.expand_contract_toggle');
     if (content.is(":visible")) {
-      icon.attr("src", icon.attr("src").replace(/list_open[^.]*\.png/, "list_closed.png"));
+      icon.removeClass("umlaut_icons-list-open").addClass("umlaut_icons-list-closed");
       $(this).find(".expand_contract_action_label").text("Show ");
       content.hide();
     } else {
-      icon.attr("src", icon.attr("src").replace(/list_closed[^.]*\.png/, "list_open.png"));
+      icon.removeClass("umlaut_icons-list-closed").addClass("umlaut_icons-list-open");
       $(this).find(".expand_contract_action_label").text("Hide ");
       content.show();
     }
