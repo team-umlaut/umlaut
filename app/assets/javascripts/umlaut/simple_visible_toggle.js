eb93fb52285d29a246ddabda7195707497e7f01e
Jonathan Rochkind <jonathan@dnil.net>
2013-02-20 17:40:06 -0500

    replace all Jquery .live with .on, for jquery 1.9 compat. issue #11

--- app/assets/javascripts/umlaut/simple_visible_toggle.js
+++ app/assets/javascripts/umlaut/simple_visible_toggle.js
@@ -1,7 +1,7 @@
 /* simple_visible_toggle.js.  Used for toggling visibility of error information. Can possibly be combined with more powerful expand_contract_toggle.js */
 jQuery(document).ready(function($) {
    
-    $("a.simple_visible_toggle").live("click", function() {
+    $(document).on("click", "a.simple_visible_toggle", function() {
        $(this).next().toggle(); 
     });
     
