9435a54a877a182d7b34ea0ee19a328132946723
Scot Dalton <scotdalton@gmail.com>
2013-03-14 12:28:45 -0400

    Cleanup whitespace in error_handling gem.

--- app/controllers/umlaut/error_handling.rb
+++ app/controllers/umlaut/error_handling.rb
@@ -6,26 +6,25 @@ module Umlaut::ErrorHandling
   included do
     # Only custom errors in production
     unless  Rails.application.config.consider_all_requests_local
-
       # generic catch-all comes first, later ones will take priority
       rescue_from Exception, :with => :handle_general_error
       rescue_from ActiveRecord::RecordNotFound, :with => :handle_404_error
     end
   end
 
-  protected
-
   def handle_general_error(exception)
     log_error_with_context(exception)
     @page_title = "Error!"
     render "error", :status => 500 unless performed?
   end
+  protected :handle_general_error
 
   # Just returns a generic 404 page.
   # Uses generic 404 page already stored in public/404.html as rails convention.
   def handle_404_error(exception=nil)
     render :file=>File.join(Rails.root ,"public/404"), :layout=>false, :status=>404
   end
+  protected :handle_404_error
 
   def log_error_with_context(exception, severity = :fatal)
     message = "\n#{exception.class} (#{exception.message}):\n"
@@ -40,4 +39,5 @@ module Umlaut::ErrorHandling
     message << "  " << Rails.backtrace_cleaner.clean(exception.backtrace, :noise).join("\n  ")
     logger.send(severity, "#{message}\n\n")
   end
+  protected :log_error_with_context
 end
