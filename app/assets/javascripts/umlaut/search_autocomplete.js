c4f783edd9350ea1fb99ae52b1fe948926c7cb9c
Jonathan Rochkind <jonathan@dnil.net>
2013-04-04 11:51:56 -0400

    autocomplete only look up on more than 2 chars

--- app/assets/javascripts/umlaut/search_autocomplete.js
+++ app/assets/javascripts/umlaut/search_autocomplete.js
@@ -33,18 +33,21 @@ jQuery(document).ready(function($) {
     }
   });
 
-  // Search for the title with the current form.
+  // Search for the title with the current form. Only search
+  // if there are more than two chars though!
   var search_title = function(query, process) {
-    var form = this.$element.closest("form");
-    var url = form.attr("action").replace("journal_search", "auto_complete_for_journal_title");
-    // Get JSON from 
-    $.getJSON(
-      form.attr("action").replace("journal_search", "auto_complete_for_journal_title"),
-      form.serialize(),
-      function(data) {
-        process(data)
-      }
-    )
+    if (query.length > 2) {
+      var form = this.$element.closest("form");
+      var url = form.attr("action").replace("journal_search", "auto_complete_for_journal_title");
+      // Get JSON from 
+      $.getJSON(
+        form.attr("action").replace("journal_search", "auto_complete_for_journal_title"),
+        form.serialize(),
+        function(data) {
+          process(data)
+        }
+      )
+    }
   }
 
   $("input.title_search").typeahead({
