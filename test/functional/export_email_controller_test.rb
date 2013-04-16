b8acec8a39f2857543d000a948ec89c30ad146b1
Jonathan Rochkind <jonathan@dnil.net>
2013-04-16 15:35:05 -0400

    test for email export controller, strangely passing even though we're having problems in reality

--- test/functional/export_email_controller_test.rb
+++ test/functional/export_email_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
+
+
+# The ExportEmailController provides custom interactions
+# for the ExportEmail service. 
 class ExportEmailControllerTest < ActionController::TestCase
   fixtures :requests, :referents, :referent_values, :dispatched_services, :service_responses
 
@@ -21,5 +25,19 @@ class ExportEmailControllerTest < ActionController::TestCase
     assert_select "body", 0
     assert_select "div.umlaut.container", 0
     assert_select "div.email", 1
+  end
+
+  test "send email" do
+    to_addr = "nobody@example.org"
+    post :send_email, :id => @email_service_response.id, :email => to_addr
+
+
+    # Mail was sent
+    assert !ActionMailer::Base.deliveries.empty?
+    assert ActionMailer::Base.deliveries.find do |message|
+      message.to.include? to_addr      
+    end
+
+    assert_select "div", :text => /Sent to #{to_addr}/
   end
 end
