75f814f32f39e2b6c8a1c6ba31aeba64f691715e
Jonathan Rochkind <jonathan@dnil.net>
2013-05-22 12:06:29 -0400

    move footer_credit out of render_service_credits

--- app/helpers/umlaut/footer_helper.rb
+++ app/helpers/umlaut/footer_helper.rb
@@ -11,10 +11,6 @@ module Umlaut::FooterHelper
     if @collection
       content = "".html_safe
             
-      if umlaut_config.footer_credit
-        content << umlaut_config.footer_credit + " | "
-      end
-      
       content << "Powered by ".html_safe + link_to("Umlaut", "https://github.com/team-umlaut/umlaut") + ". ".html_safe
       
       credit_segments = []
