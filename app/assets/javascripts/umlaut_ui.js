bbeff62945b0dbc5180a26bdbeb96a7b2b44be00
Jonathan Rochkind <jonathan@dnil.net>
2013-06-04 11:46:03 -0400

    umlaut_ui.js, restore external delivery of umlaut JS behaviors. VERY HACKY, may not be long term tenable

--- app/assets/javascripts/umlaut_ui.js
+++ app/assets/javascripts/umlaut_ui.js
@@ -6,10 +6,33 @@
    
    At present, only expand/contract toggle behavior is actually supported,
    others are non-applicable or hard to get working on an external site
-   due to cross-domain-origin stuff.   
-   
+   due to cross-domain-origin stuff.  
+
+   And even this has become VERY HACKY AND FRAGILE these days --
+   this whole concept may be nontenable.  
    
 *= require 'umlaut/expand_contract_toggle.js'   
-   
-   
+      
 */
+
+/* Normal umlaut uses bootstrap collapse, and expand_contract_toggle.js
+   assumes bootstrap collapse. For vended use as here, provide our own
+   simple kind of crappy replacement for bootstrap collapse, which
+   will combine with expand_contract_toggle.js above to completely implement. */
+
+jQuery(document).ready(function($) {
+	$(".collapse").hide();
+
+  $(document).on("click", ".collapse-toggle", function(event) {  	
+  	content = $( $(this).attr('data-target') );
+
+  	if ( content.is(":visible") ) {
+  		content.slideUp();
+  		content.trigger("hide");
+  	}
+  	else {
+  		content.slideDown();
+  		content.trigger("show");
+  	}
+  });
+});
