2af72c675cd6ef7a9494e53ca82530ccf8f537ae
Scot Dalton <scotdalton@gmail.com>
2014-01-06 10:57:20 -0500

    Remove query string from umlaut_base if a query string was passed into the HtmlUpdater.  Fixes #33

--- app/assets/javascripts/umlaut/update_html.js
+++ app/assets/javascripts/umlaut/update_html.js
@@ -48,6 +48,9 @@
     if (context_object == undefined)
       context_object = "";      
     
+    // Remove query string (if present)
+    umlaut_base = umlaut_base.replace(/\?.*$/, '')
+    // Remove trailing slash
     umlaut_base = umlaut_base.replace(/\/$/,'');
     this.umlaut_uri =  umlaut_base + '/resolve/partial_html_sections?umlaut.response_format=json&' + context_object;
         
