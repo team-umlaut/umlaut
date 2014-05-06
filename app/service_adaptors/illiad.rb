0719f1416f44bb0605588a94ea0b5a283a3a3efd
Jonathan Rochkind <jonathan@dnil.net>
2014-05-06 16:53:11 -0400

    ILLiad service adapter

---
+++ app/service_adaptors/illiad.rb
@@ -0,0 +1,132 @@
+require 'cgi'
+require 'openurl'
+
+# Just creates an OpenURL link out, corresponding to the current OpenURL. 
+# But tweaked in ways to try and work out for sending to ILLiad. 
+#
+# If you use SFX, you may want to just use SFX's built-in ILLiad targets, which
+# will generally be picked up by the SFX Umlaut service. 
+#
+# But if you don't or if you are unhappy with what SFX is doing, you could turn
+# off ILLiad target(s) in SFX (or configure Umlaut SFX adapter to ignore them), 
+# and use this instead. 
+#
+# # Config parameters
+# ## Required
+# 
+# * base_url: Illiad base url, such as `http://ill.university.edu/site/illiad.dll/OpenURL`. It should probably end in '/illiad.dll/OpenURL'
+#
+# ## Optional
+# 
+# * display_name: Default "Place ILL Request"
+# * sid_suffix: Default " (via Umlaut)", appended to existing sid before sending to ILLiad. 
+# * notes: Some additional notes to display under the link. 
+class Illiad < Service
+  include MetadataHelper
+
+  required_config_params :base_url
+
+  def initialize(config)
+    @service_type = "document_delivery"
+    @display_name = "Place ILL Request"
+    @sid_suffix   = " (via Umlaut)"
+
+    super(config)    
+  end
+
+
+  def service_types_generated
+    [ServiceTypeValue[@service_type.to_sym]]
+  end
+
+  def handle(request)   
+    target_url = @base_url + "?" + illiad_query_parameters(request).to_query
+
+    request.add_service_response(
+      :service            =>self, 
+      :display_text       => @display_name,
+      :url                => target_url,
+      :notes              => @notes, 
+      :service_type_value => @service_type.to_sym
+    )
+
+    return request.dispatched(self, true)
+  end
+
+  
+  def illiad_query_parameters(request)
+    metadata = request.referent.metadata
+
+    qp = {}
+
+    qp['genre']     = metadata['genre']
+
+    if metadata['aulast']
+      qp["aulast"]  = metadata['aulast']
+      qp["aufirst"] = [metadata['aufirst'], metadata["auinit"]].find {|a| a.present?}
+    else
+      qp["au"]      = metadata["au"]
+    end
+
+    qp['volume']    = metadata['volume']
+    qp['issue']     = metadata['issue']
+
+    qp['spage']     = get_spage(request.referent)
+    qp['epage']     = get_epage(request.referent)
+
+    qp['issn']      = get_issn(request.referent)
+    qp['isbn']      = get_isbn(request.referent)
+    qp['pmid']      = get_pmid(request.referent)
+
+    qp['stitle']    = metadata['stitle']
+
+    qp['sid']       = sid_for_illiad(request)
+
+    qp['year']      = get_year(request.referent)
+    qp['month']     = get_month(request.referent)
+
+    qp['atitle']    = metadata['atitle']
+
+    # ILLiad always wants 'title', not the various title keys that exist in OpenURL
+    qp['title']     = [metadata['jtitle'], metadata['btitle'], metadata['title']].find {|a| a.present?}
+
+    # For some reason these go to ILLiad prefixed with rft.
+    qp['rft.pub']     = metadata['pub']
+    qp['rft.place']   = metadata['place']
+    qp['rft.edition'] = metadata['edition']
+
+    # ILLiad likes OCLCnum in `rfe_dat`
+    qp['rfe_dat']   = get_oclcnum(request.referent)
+
+
+    # Genre normalization. ILLiad pays a lot of attention to `&genre`, but
+    # doesn't use actual OpenURL rft_val_fmt
+    if request.referent.format == "dissertation"
+      qp['genre'] = 'dissertation'
+    elsif qp['isbn'].present? && qp['genre'] == 'book' && qp['atitle'] && (! qp['issn'].present?)
+      # actually a book chapter, not a book, fix it. 
+      qp['genre'] = 'bookitem'
+    elsif qp['issn'].present? && qp['atitle'].present?
+      # Otherwise, if there is an ISSN, we force genre to 'article', seems
+      # to work best.  
+      qp['genre'] = 'article'      
+    elsif qp['genre'] == 'unknown' && qp['atitle'].blank?
+      # WorldCat likes to send these, ILLiad is happier considering them 'book'
+      qp['genre'] = "book"
+    end
+
+    # trim empty ones please
+    qp.delete_if {|k, v| v.blank?}
+    
+    return qp
+  end
+
+  # Grab a source label out of `sid` or `rfr_id`, add on our suffix. 
+  def sid_for_illiad(request)    
+    sid = request.referrer_id || ""
+
+    sid = sid.gsub(%r{\Ainfo\:sid/}, '')
+
+    return "#{sid}#{@sid_suffix}"
+  end
+end
---
+++ test/unit/illiad_test.rb
@@ -0,0 +1,146 @@
+require 'test_helper'
+require 'uri'
+
+class IlliadTest < ActiveSupport::TestCase
+
+  def setup
+    @service       = Illiad.new('service_id' => 'test_illiad', 'base_url' => 'http://example.org/illiad.dll/OpenURL', 'priority' => 0)
+  end
+
+  def make_test_request(params)  
+    # hard to figure out how to mock a request, this seems to work
+    ActionController::TestRequest.new(Rack::MockRequest.env_for("?#{params.to_query}"))    
+  end
+
+  def execute_service(params)
+    rails_request = make_test_request(params)
+    umlaut_request = Request.find_or_create(params, {}, rails_request)
+    @service.handle_wrapper(umlaut_request)
+    return umlaut_request
+  end
+
+  def parsed_params_from_first_response(umlaut_request)
+    response = umlaut_request.service_responses.first
+    target_url = response.url
+    target_url =~ %r{\Ahttp://example.org/illiad.dll/OpenURL\?(.*)\Z}
+    query_string = $1
+    out_params = CGI.parse(query_string)
+
+    return out_params
+  end
+
+  def test_basic_case
+    params = {
+      'sid'     => 'SomeSource',
+      'genre'   => 'article',
+      'aulast'  => 'Smith',
+      'aufirst' => 'John',
+      'au'      => 'Smith, Johnny, Jr., Esq.',
+      'volume'  => '100',
+      'issue'   => '1',
+      'spage'   => '500',
+      'epage'   => '505',
+      'pages'   => 'do not use',
+      'issn'    => '12345678',
+      'jtitle'  => 'Journal Of Lots Of Things',
+      'stitle'  => 'J L Things',
+      'atitle'  => 'Article Title',
+      'date'    => '2011-05-12',
+      'pub'     => 'Johnson Publishing',
+      'place'   => 'Baltimore, MD',
+      'edition' => '1st ed.'
+    }
+
+    umlaut_request = execute_service(params)
+    
+
+    assert_length 1, umlaut_request.service_responses
+
+    response = umlaut_request.service_responses.first
+
+    assert_equal "document_delivery", response.service_type_value_name
+
+    target_url = response.url
+    assert_present target_url
+    assert_match %r{\Ahttp://example.org/illiad.dll/OpenURL\?(.*)\Z}, target_url
+
+
+    target_url =~ %r{\Ahttp://example.org/illiad.dll/OpenURL\?(.*)\Z}
+    query_string = $1
+    out_params = CGI.parse(query_string)
+    
+    assert_equal ['SomeSource (via Umlaut)'], out_params['sid']
+
+    # Ones that just get passed through
+    ['genre', 'aulast', 'aufirst', 'volume', 'issue', 'spage', 'epage', 
+      'issn', 'stitle', 'atitle'].each do |key|
+        assert_equal [params[key]], out_params[key]
+    end
+
+    assert_equal ['2011'], out_params['year']
+    assert_equal ['05'], out_params['month']
+
+    assert_equal [params['jtitle']], out_params['title']
+
+    assert_equal [params['pub']], out_params['rft.pub']
+    assert_equal [params['place']], out_params['rft.place']
+    assert_equal [params['edition']], out_params['rft.edition']
+
+    assert_empty out_params['au']
+  end
+
+  def test_genre_dissertation_normalize    
+    params = {
+      'rft_val_fmt'=> 'info:ofi/fmt:kev:mtx:dissertation',
+      'title' => 'our title',
+      'au' => 'our author'
+    }
+    umlaut_request = execute_service(params)
+    out_params = parsed_params_from_first_response(umlaut_request)  
+
+    assert_equal ['dissertation'], out_params['genre']
+    assert_equal ['our title'], out_params['title']
+    assert_equal ['our author'], out_params['au']
+  end
+
+  def test_genre_bookitem_normalize
+    params = {
+      'genre' => 'book',
+      'atitle' => 'Chapter Title',
+      'title' => 'Book Title',
+      'isbn'  => '978-3-16-148410-0'
+    }
+    umlaut_request = execute_service(params)
+    out_params = parsed_params_from_first_response(umlaut_request)  
+
+    assert_equal ['bookitem'], out_params['genre']
+    assert_equal [params['atitle']], out_params['atitle']
+    assert_equal [params['title']], out_params['title']
+    assert_equal [params['isbn']], out_params['isbn']
+  end
+
+  def test_genre_force_issn_article
+    params = {
+      'atitle' => "A title",
+      'jtitle' => 'Journal Title',
+      'issn'  => '12345678'
+    }
+
+    umlaut_request = execute_service(params)
+    out_params = parsed_params_from_first_response(umlaut_request)  
+
+    assert_equal ['article'], out_params['genre']
+  end
+
+  def test_genre_force_book
+    params = {
+      'title' => 'DVD Title?',
+      'genre' => 'unknown',
+      'isbn' => '978-3-16-148410-0'
+    }
+    umlaut_request = execute_service(params)
+    out_params = parsed_params_from_first_response(umlaut_request)  
+
+    assert_equal ['book'], out_params['genre']
+  end
+end
