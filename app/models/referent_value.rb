295d376c4ed8240178b706f004f188df14796d17
Jonathan Rochkind <jonathan@dnil.net>
2014-07-10 11:16:01 -0400

    remove attr_accessible for rails4

--- app/models/referent_value.rb
+++ app/models/referent_value.rb
@@ -1,5 +1,4 @@
 class ReferentValue < ActiveRecord::Base
-  attr_accessible :key_name, :value, :normalized_value, :metadata, :private_data
   belongs_to :referent, :include => :referent_values
 
   # Class method to normalize a string for normalized_value attribute. 
