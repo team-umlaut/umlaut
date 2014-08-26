47b639e250eb9d5eaa09e3d67faed819d2fb73e5
Jonathan Rochkind <jonathan@dnil.net>
2014-08-26 17:47:25 -0400

    Something really weird was going on with the internet archive test, I can't even explain it, but abandon fixtures please and thank you

--- test/unit/internet_archive_test.rb
+++ test/unit/internet_archive_test.rb
@@ -4,14 +4,14 @@
 require 'test_helper'
 class InternetArchiveTest < ActiveSupport::TestCase  
   extend TestWithCassette
-  fixtures :requests, :referents, :referent_values
-  
+
   def setup    
     @ia_default = ServiceStore.instantiate_service!("InternetArchive", nil)
   end
 
-  test_with_cassette("momo by title author", :internet_archive) do
-    request = requests(:momo)
+  test_with_cassette("momo by title author", :internet_archive) do    
+    request = fake_umlaut_request("/resolve?isbn=038519093X&aufirst=Michael&aulast=Ende&btitle=Momo&genre=book&isbn=038519093X&pub=Doubleday")
+    
     # Clear out the current responses.
     request.service_responses.each do |service_response|
       service_response.destroy
@@ -28,6 +28,7 @@ class InternetArchiveTest < ActiveSupport::TestCase
       fulltext.url, "IA Service constructed an unexpected URL")
     view_data = fulltext.view_data
     assert((not view_data.nil?), "IA full text view data is nil")
-    assert_equal("the Internet Archive: Open Source Book", view_data[:display_text], "IA display text is unexpected")
+
+    assert_equal("the Internet Archive: Open Source Books", view_data[:display_text], "IA display text is unexpected")
   end
 end
