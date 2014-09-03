4c973bba862efe50a6eddffd900a03298c727d53
Jonathan Rochkind <jonathan@dnil.net>
2014-09-03 10:25:19 -0400

    more efficient creation of referents

--- test/unit/referent_test.rb
+++ test/unit/referent_test.rb
@@ -4,18 +4,10 @@ require 'test_helper'
 class ReferentTest < ActiveSupport::TestCase
 
 
-  def test_ensure_value
-    referent = Referent.new
-    referent.save!
-
-    referent.ensure_value!("title", "this is a title")  
-    assert_present (referent.referent_values(true).to_a.find {|rv| rv.key_name = "title" && rv.value == "this is a title"}), "referent value not saved"
-  end
-
   def test_enhance_referent
     referent = Referent.new
+    referent.build_referent_value("title", "this is a title")  
     referent.save!
-    referent.ensure_value!("title", "this is a title")  
 
     another_reference = Referent.find(referent.id)
     refute_same referent, another_reference
@@ -37,9 +29,8 @@ class ReferentTest < ActiveSupport::TestCase
   end
 
   def test_create_by_context_object
-    url = "?url_ver=Z39.88-2004&url_ctx_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Actx&ctx_ver=Z39.88-2004&ctx_tim=2014-09-03T09%3A56%3A22-04%3A00&ctx_id=&ctx_enc=info%3Aofi%2Fenc%3AUTF-8&rft.genre=article&rft.au=Chen%2C+Yu-Hui&rft.date=2009-05-01&rft.volume=60&rft.issue=5&rft.spage=953&rft.jtitle=Journal+of+the+American+Society+for+Information+Science+%26+Technology&rft.issn=15322882&rft.pub=Wiley-Blackwell&rft.atitle=An+exploration+into+the+practices+of+library+Web+usability+in+ARL+academic+libraries.&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft_id=info%3Adoi%2F10.1002%2Fasi.21032&rfr_id=http%3A%2F%2Fcatalyst.library.jhu.edu%2Fsearch%2Farticles"
-    co  = OpenURL::ContextObject.new_from_form_vars(Request.context_object_params(fake_rails_request( url )))
-    
+    url = "url_ver=Z39.88-2004&url_ctx_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Actx&ctx_ver=Z39.88-2004&ctx_tim=2014-09-03T09%3A56%3A22-04%3A00&ctx_id=&ctx_enc=info%3Aofi%2Fenc%3AUTF-8&rft.genre=article&rft.au=Chen%2C+Yu-Hui&rft.date=2009-05-01&rft.volume=60&rft.issue=5&rft.spage=953&rft.jtitle=Journal+of+the+American+Society+for+Information+Science+%26+Technology&rft.issn=15322882&rft.pub=Wiley-Blackwell&rft.atitle=An+exploration+into+the+practices+of+library+Web+usability+in+ARL+academic+libraries.&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft_id=info%3Adoi%2F10.1002%2Fasi.21032&rfr_id=http%3A%2F%2Fcatalyst.library.jhu.edu%2Fsearch%2Farticles"
+    co  = OpenURL::ContextObject.new_from_kev(url)    
     rft = Referent.create_by_context_object(co)
 
     assert rft.persisted?
