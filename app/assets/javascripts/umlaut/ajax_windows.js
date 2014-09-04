6562985988a4e16e85483af0c6fe1109376e7a28
Jonathan Rochkind <jonathan@dnil.net>
2014-09-04 13:53:05 -0400

    fix email modal for bootstrap3

--- app/assets/javascripts/umlaut/ajax_windows.js
+++ app/assets/javascripts/umlaut/ajax_windows.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function($) {
   var populate_modal = function(data, textStatus, jqXHR) {
     // Wrap the data object in jquery object
-    var body = $(data);
+    var body = $("<div/>").html(data);
     // Remove the first heading from the returned data
     var header = body.find("h1, h2, h3, h4, h5, h6").eq(0).remove();
     // Remove the first submit button from the returned data
@@ -14,18 +14,14 @@ jQuery(document).ready(function($) {
     body = arguments[1]
     footer = arguments[2]
     // Replace the header text if given
-    if (header) $("#modal").find(".modal-header").find(".content").text(header.text());
+    if (header) $("#modal").find(".modal-header").find("[data-role=modal-title-content]").text(header.text());
     // Replace the body html if given
-    if (body) $("#modal").find(".modal-body").find(".content").html(body.html());
+    if (body) $("#modal").find(".modal-body").find("[data-role=modal-body-content]").html(body.html());
     // Replace the current submit button if given
-    if (footer) $("#modal").find(".modal-footer").find(".content").html(footer);
+    if (footer) $("#modal").find(".modal-footer").find("[data-role=modal-footer-content]").html(footer);
     // Toggle the ajax-loader
     $("#modal").find(".modal-header").find(".ajax-loader").toggle();
     $("#modal").find(".modal-body").find(".ajax-loader").toggle();
-    // Toggle the content
-    $("#modal").find(".modal-header").find(".content").toggle();
-    $("#modal").find(".modal-body").find(".content").toggle();
-    $("#modal").find(".modal-footer").find(".content").toggle();
   }
   var display_modal = function(event) {
     event.preventDefault();
