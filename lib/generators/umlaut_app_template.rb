38b559b162a36ed2bff80e4398f90127ac59fad0
Jonathan Rochkind <jonathan@dnil.net>
2014-09-15 13:03:40 -0400

    umlaut app template with command line generation

--- lib/generators/umlaut_app_template.rb
+++ lib/generators/umlaut_app_template.rb
@@ -1,3 +1,14 @@
+# An app template to set up a Rails app with umlaut, with required
+# configuration and generated content. 
+
+# ENV['UMLAUT_GEM_PATH'] can be used to add the line to Gemfile
+# with a path to local checkout of Umlaut. NOTE WELL the path
+# passed in ENV must be absolute path OR relative to the generated
+# app's gemfile (which is confusing). (We've already lost the actual
+# CWD by the the time the generator runs, so we can't correct to relative
+# to original command CWD)
+# 
+
 # Add the lib path from the copy of Umlaut we're in to the load path. 
 # Since we're an app template, our gem isn't loaded yet. 
 umlaut_lib = File.expand_path(File.dirname(__FILE__) + '/../')
@@ -7,9 +18,13 @@ require 'umlaut/version'
 
 umlaut_version = Umlaut::VERSION.split(".")
 
-
 gem_spec_str = 
   "\ngem 'umlaut', '>= #{Umlaut::VERSION}', '< #{umlaut_version.first.to_i + 1}'"
+if ENV["UMLAUT_GEM_PATH"]
+  path = File.expand_path( ENV["UMLAUT_GEM_PATH"] )
+  gem_spec_str += ", :path => '#{path}'"
+end
+
 
 append_file "Gemfile", gem_spec_str
 
