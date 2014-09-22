f26735772b74a71f3cb1d57220e64ee167655d42
Jonathan Rochkind <jonathan@dnil.net>
2014-09-22 13:51:15 -0400

    bump version"

--- lib/umlaut/version.rb
+++ lib/umlaut/version.rb
@@ -1,8 +1,8 @@
 module Umlaut
-  VERSION = "4.0.0.beta3"
+  VERSION = "4.0.0.beta4"
 
   # This is used in Umlaut's .gemspec for generating the gem,
   # and is also used in the umlaut app generator to make sure
   # we're generating with a compatible Rails version. 
-  RAILS_COMPAT_SPEC = [">= 3.2.12", "< 4.2.0.beta1"]
+  RAILS_COMPAT_SPEC = [">= 3.2.12", "<= 4.2.0.beta1"]
 end
