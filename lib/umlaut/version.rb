eb909e029ac61d815a05b45f29dac438fde876e2
Jonathan Rochkind <jonathan@dnil.net>
2014-09-29 22:11:02 -0400

    bump beta version

--- lib/umlaut/version.rb
+++ lib/umlaut/version.rb
@@ -1,5 +1,5 @@
 module Umlaut
-  VERSION = "4.0.0.beta4"
+  VERSION = "4.0.0.beta5"
 
   # This is used in Umlaut's .gemspec for generating the gem,
   # and is also used in the umlaut app generator to make sure
