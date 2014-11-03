74062e3ab156bc92542582a7352b7e4badb22493
Jonathan Rochkind <jonathan@dnil.net>
2014-11-03 13:13:40 -0500

    mark 4.0.3 with i18n_notes bugfix

--- lib/umlaut/version.rb
+++ lib/umlaut/version.rb
@@ -1,5 +1,5 @@
 module Umlaut
-  VERSION = "4.0.2"
+  VERSION = "4.0.3"
 
   # This is used in Umlaut's .gemspec for generating the gem,
   # and is also used in the umlaut app generator to make sure
