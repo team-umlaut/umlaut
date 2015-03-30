1a8950c075cec6d51f7d3894cbbc3d2b65935ccb
Jonathan Rochkind <jonathan@dnil.net>
2015-03-30 10:58:19 -0400

    bump version 4.1.0 pre 3
    Sorry for the different formats for pre versions, trying to make
    it sort right AND work non-confusingly with rubygems which
    didn't like the first thing we did based on semver docs.

--- lib/umlaut/version.rb
+++ lib/umlaut/version.rb
@@ -1,5 +1,5 @@
 module Umlaut
-  VERSION = "4.1.0.pre.2"
+  VERSION = "4.1.0.pre3"
 
   # This is used in Umlaut's .gemspec for generating the gem,
   # and is also used in the umlaut app generator to make sure
